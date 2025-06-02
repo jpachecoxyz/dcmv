565cf947a976f685056cb0542c12089b6ce8ebef
Javier Pacheco <jpacheco@cock.li>
2025-06-01 20:08:47 -0500

    Video recorder for face and fullbody

--- main.py
+++ main.py
@@ -1,30 +1,64 @@
 import cv2
+import time
+import datetime
 
-# Open the default camera
-cam = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
-# Get the default frame width and height
-frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+body_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_fullbody.xml")
 
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
+detection = False
+detection_stopped_time = None
+timer_started = False
+SECONDS_TO_RECORD_AFTER_DETECTION = 5
+
+frame_size = (int(cap.get(3)), int(cap.get(4)))
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
 
 while True:
-    ret, frame = cam.read()
+    _, frame = cap.read()
 
-    # Write the frame to the output file
-    out.write(frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    bodies = body_cascade.detectMultiScale(gray, 1.3, 5)
 
-    # Display the captured frame
-    cv2.imshow('Camera', frame)
+    # for (x, y, width, height) in faces:
+    #     cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3)
 
-    # Press 'q' to exit the loop
+    for (x, y, width, height) in bodies:
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3)
+
+    if len(faces) + len(bodies) > 0:
+    # if len(bodies) > 0:
+        if detection:
+            timer_started = False
+        else:
+            detection = True
+            current_time = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+            out = cv2.VideoWriter(f"{current_time}.mp4", fourcc, 20.0, frame_size)
+            print("Started Recording!")
+    elif detection:
+        if timer_started:
+            if time.time() - detection_stopped_time >= SECONDS_TO_RECORD_AFTER_DETECTION:
+                detection = False
+                timer_started = False
+                out.release()
+                print("Stop Recording!")
+        else:
+            timer_started = True
+            detection_stopped_time = time.time()
+
+
+    if detection:
+        out.write(frame)
+
+    cv2.imshow("DCM_1600", frame)
+
     if cv2.waitKey(1) == ord('q'):
         break
 
-# Release the capture and writer objects
-cam.release()
-out.release()
+# out.release()
+cap.release()
 cv2.destroyAllWindows()
